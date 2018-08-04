ab0229397d9a7e0a6bcbef2a5a3e29aee046dc8e
NilSagor <masum1201@gmail.com>
2018-08-04 13:02:45 +0800

    fix new_topic templates

--- django_forum/urls.py
+++ django_forum/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
 	path('', views.home, name = 'home'),
 	path('boards/<int:pk>/', views.board_topics, name = 'board_topics'),
 	path('boards/<int:pk>/new/', views.new_topic, name ='new_topic'),
-	path('signup/', accounts_views.signup, name = 'signup')
+	path('signup/', accounts_views.signup, name = 'signup'),
     path('admin/', admin.site.urls),
 ]
