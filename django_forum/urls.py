1488766a92e3cd537d028d426490e0a06cd744bf
NilSagor <masum1201@gmail.com>
2018-08-05 09:44:58 +0800

    login teplate

--- django_forum/urls.py
+++ django_forum/urls.py
@@ -15,14 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 from djforum import views
 from accounts import views as accounts_views
+
+
 
 urlpatterns = [
 	path('', views.home, name = 'home'),
 	path('boards/<int:pk>/', views.board_topics, name = 'board_topics'),
 	path('boards/<int:pk>/new/', views.new_topic, name ='new_topic'),
 	path('signup/', accounts_views.signup, name = 'signup'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
+	path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
     path('admin/', admin.site.urls),
 ]
