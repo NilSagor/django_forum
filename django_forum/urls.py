df354c7b9299cb9359e345e384cfc280cf7cbc15
NilSagor <masum1201@gmail.com>
2018-08-09 01:36:18 +0800

    url path change to re_path for regular expression

--- django_forum/urls.py
+++ django_forum/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 
 
@@ -39,7 +39,7 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name = 'password_reset_done.html'),
         name = 'password_reset_done'),
-    path('reset/<?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z_]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path('^(reset/<?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z_]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(
             template_name = 'password_reset_confirm.html'),
             name = 'password_reset_confirm'),
