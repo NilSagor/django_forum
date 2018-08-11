1cc982c2b3e3baa8d0dca9a33aceef5a071d09e4
NilSagor <masum1201@gmail.com>
2018-08-11 16:29:57 +0800

    login required test added

--- djforum/tests/test_view_new_topic.py
+++ djforum/tests/test_view_new_topic.py
@@ -89,4 +89,16 @@ class NewTopicTests(TestCase):
 		self.assertFalse(Post.objects.exists())
 
 
+class LoginRequiredNewTopicTests(TestCase):
+	def setUp(self):
+		Board.objects.create(name = 'Django', description='Django board')
+		self.url = reverse('new_topic', kwargs={'pk': 1})
+		self.response = self.client.get(self.url)
+
+	def test_redirect(self):
+		login_url = reverse('login')
+		self.assertRedirects(
+			self.response, 
+			'{login_url}?next={url}'.format(login_url=login_url, url=self.url))
+
 	
